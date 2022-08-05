965326d051ddf1bb4c71bcfa3f733087b23319c3
Derson Mutemba <55860970+dersonmutemba@users.noreply.github.com>
2022-08-05 23:55:54 +0200

    Menu

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -5,8 +5,6 @@ class Menu extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('menu'),
-    );
+    return ListView();
   }
 }
